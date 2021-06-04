32ce28809e64566b2bf44e3fef8c91a35db2d24a
unknown <kjaehwan89@gmail.com>
2021-06-03 20:34:52 -0400

    first commit

---
+++ src/components/TOC.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class TOC extends Component {
+  shouldComponentUpdate(newProps, newStates) {
+    console.log(
+      "TOC render shouldComponentUpdate",
+      this.state,
+      newStates
+    );
+    if (this.props.data === newProps.data && this.state === newStates) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  render() {
+    console.log("TOC render");
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+          >
+            {data[i].title}
+          </a>
+        </li>
+      );
+      i = i + 1;
+    }
+    return (
+      <nav>
+        <ul>{lists}</ul>
+      </nav>
+    );
+  }
+}
+
+export default TOC;
